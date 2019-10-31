fdfdc1ffc6e21ce7a03dcc3e29f3d87a2e74d56a
tolentino <tolentino@192.168.0.103>
2019-10-31 12:22:40 -0200

    Crud funcional de pessoa

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?
 
+//PRINT_RP
 function pr($dado) {
     echo '<pre>';
     print_r($dado);
@@ -9,38 +10,68 @@ function pr($dado) {
 $PDO = new PDO('mysql:host=127.0.0.1;dbname=CRUD', 'root', '');
 
 //INCLUIR
-/*
-  $pesIncluir = $PDO->prepare('INSERT INTO PESSOA (NOME) VALUES(:NOME)');
-  $pesIncluir->execute(array(
-  ':NOME' => 'Marcos Tolentino da Rosa'
-  ));
- */
-
-/*
-  //ALTERAR
-  $pesAlterar = $PDO->prepare('UPDATE PESSOA SET NOME = :NOME WHERE ID_PESSOA = :ID_PESSOA');
-  $pesAlterar->execute(array(
-  ':NOME' => 'Não é mais marcos tolentino',
-  ':ID_PESSOA' => '1'
-  ));
- */
-
-
+if (@$_POST['ACAO'] == 'Incluir') {
+    $pesIncluir = $PDO->prepare('INSERT INTO PESSOA (NOME) VALUES(:NOME)');
+    $pesIncluir->execute(array(
+        ':NOME' => $_POST['NOME']
+    ));
+}
+//ALTERAR
+elseif (@$_POST['ACAO'] == 'Alterar') {
+    $pesAlterar = $PDO->prepare('UPDATE PESSOA SET NOME = :NOME WHERE ID_PESSOA = :ID_PESSOA');
+    $pesAlterar->execute(array(
+        ':NOME' => $_POST['NOME'],
+        ':ID_PESSOA' => $_POST['ID_PESSOA']
+    ));
+}
 //EXCLUIR
-$pesAlterar = $PDO->prepare('DELETE FROM PESSOA WHERE ID_PESSOA = :ID_PESSOA');
-$pesAlterar->execute(array(
-    ':ID_PESSOA' => '1'
-));
+elseif (@$_GET['ACAO'] == 'Excluir') {
+    $pesAlterar = $PDO->prepare('DELETE FROM PESSOA WHERE ID_PESSOA = :ID_PESSOA');
+    $pesAlterar->execute(array(
+        ':ID_PESSOA' => $_GET['ID_PESSOA']
+    ));
+}
+?>
 
+<center>
+    <table border="1" style="min-width: 500px">
+        <tr style=" vertical-align: top">
+            <td style="text-align: right;">
+                <h1 style="text-align: center">Listar Pessoas</h1> 
 
-//LISTAR
-$sql = "
-    SELECT P.ID_PESSOA, 
-           P.NOME AS PES_NOME
-      FROM PESSOA P
-";
-$pesListar = $PDO->query($sql);
-echo '<h1>Lista de pessoas</h1>';
-while ($pes = $pesListar->fetch(PDO::FETCH_ASSOC)) {
-    echo "$pes[ID_PESSOA] - $pes[PES_NOME]<br>";
-}    
+                <?
+                $sql = "
+                    SELECT ID_PESSOA, 
+                           NOME
+                      FROM PESSOA
+                  ORDER BY NOME
+                ";
+                $pessoaQuery = $PDO->query($sql);
+                $pessoaArray = [];
+                while ($pessoaFetch = $pessoaQuery->fetch(PDO::FETCH_ASSOC)) {
+                    $pessoaArray[$pessoaFetch['ID_PESSOA']] = $pessoaFetch;
+                    echo "$pessoaFetch[ID_PESSOA] - $pessoaFetch[NOME]";
+                    ?>
+                    <form style="display: inline">
+                        <input name="ID_PESSOA" value="<?= @$pessoaFetch[ID_PESSOA] ?>" hidden>
+                        <input name="ACAO" value="Alterar" type="submit">
+                        <input name="ACAO" value="Excluir" type="submit">
+                    </form>
+                    <hr>
+                    <?
+                }
+                $pessoaAlterar = @$pessoaArray[@$_GET['ID_PESSOA']];
+                $acaoDescricao = ($pessoaAlterar ? 'Alterar' : 'Incluir');
+                ?>
+            </td>
+            <td style="text-align: center;">
+                <h1><?= $acaoDescricao ?> Pessoa</h1> 
+                <form method="POST">
+                    <input name="ID_PESSOA" value="<?= $pessoaAlterar['ID_PESSOA'] ?>" hidden>
+                    <input name="NOME" value="<?= $pessoaAlterar['NOME'] ?>" maxlength="100" required><br>
+                    <input name="ACAO" value="<?= $acaoDescricao ?>" type="submit">
+                </form>
+            </td>
+        </tr>
+    </table>
+</center>
